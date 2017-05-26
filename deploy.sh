7b1a0d10e59f7bd34350cb7099fbc655cdd13d41
maxim1812 <ppppp.ppppppp2018@yandex.ru>
2017-05-27 01:25:35 +0300

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-echo "Start deploy.sh..."
-docker login --username=_ --password=$(echo $API_KEY) registry.heroku.com
-docker build -t registry.heroku.com/$1/web .
-docker push registry.heroku.com/$1/web
-echo "deploy.sh ended"
